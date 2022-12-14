189553c6abd62393cabb98b24cfa4c3e82e1784b
Patrick <zihexiao@uw.edu>
2022-12-14 12:33:56 -0800

    update

--- portfolioManager/meanVariance/meanVariance.py
+++ portfolioManager/meanVariance/meanVariance.py
@@ -6,6 +6,19 @@ from pypfopt.efficient_frontier import EfficientFrontier
 
 # input: a list of tickers and expected return, return a dictionary with each ticker and weight
 def calculate_weight(tickers, er):
+    '''
+    Given the portfolio components stock tickers and user's expected return,
+    calculate and return the weight of each stock in portfolio.
+
+    Parameters:
+        tickers: a list of stock tickers that is selected for portfolio
+        er: user's input expected return
+    Return:
+        A dictionary of stock-weight pair.
+        Raise ValueError if larger than 1 return is passed.
+    '''
+    if er >=  1:
+        raise ValueError("Annual return cannout be larger than 1")
     df = yf.download(tickers=tickers, period="max", interval="1d")['Close'].iloc[-30:, :]
     mu = mean_historical_return(df)
     S = CovarianceShrinkage(df).ledoit_wolf()
--- portfolioManager/tests/test_meanVariance.py
+++ portfolioManager/tests/test_meanVariance.py
@@ -6,8 +6,11 @@ Class:
 Function:
   test_smoke(): a smoke test to check if MeanVariance can work without systax and runtime
                 error.
-  test_one_shot_1(), test_one_shot_2(): two one shot tests to check if knn
-                                        has the correct output.
+  test_edge_large_return: an edge test to check if large return could be handled.
+
+As Mean-Variance model compute by real-time stock price, and it may change dramatically
+each day, it is hard to write one-shot test to see if output portfolio weight matches our expectation.
+Therefore, we do not write any one-shot test here.
 """
 
 import unittest
@@ -34,20 +37,16 @@ class TestMV(unittest.TestCase):
 
 
 
-    def test_one_shot_1(self):
+    def test_edge_large_return(self):
         """
-        One shot test using the known case of the given input data.
-        Should return a dict the same as the known result.
+        One edge test to check if impossibly large return could be handled properly.
         """
         tickers = ["MRNA", "PFE", "JNJ", "GOOGL",
                    "AAPL", "COST", "WMT", "KR", "JPM",
                   "BAC"]
-        er = 0.5
-        result = {'AAPL': 0.0, 'BAC': 0.05122, 'COST': 0.05085, 'GOOGL': 0.0, 'JNJ': 0.20852,
-                  'JPM': 0.1215, 'KR': 0.19507, 'MRNA': 0.01429, 'PFE': 0.21176, 'WMT': 0.1468}
-        target  = calculate_weight(tickers, er)
-        for i in result:
-            assert np.isclose(result[i], target[i])
+        er = 1
+        with self.assertRaises(ValueError):
+           calculate_weight(tickers, er)
 
     def test_one_shot_2(self):
         """
