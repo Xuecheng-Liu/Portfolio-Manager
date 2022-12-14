a8a4437bd3a884dc6a086120c2811cf96a26cf25
Xuecheng-Liu <54543766+Xuecheng-Liu@users.noreply.github.com>
2022-12-14 06:45:23 -0800

    Update train.py

--- portfolioManager/train.py
+++ portfolioManager/train.py
@@ -5,3 +5,8 @@ from visualization import plot
 ticker_list = ["XLB","XLC","XLE","XLF","XLI","XLK","XLP","XLRE","XLU","XLV","XLY"]
 for ticker in ticker_list:
   plot(xgb_model(ticker))
+
+  
+ticker_list = ['DOW', 'APD', 'GOOGL', 'T', 'PXD']
+weight_list = [0.13, 0.28, 0.45, 0.10, 0.04]
+backtest(ticker_list, weight_list)
